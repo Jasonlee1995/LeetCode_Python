c39dd6d03877d8fe9cecec2213aa4097f2b9e3e3
Jason <2014145110@yonsei.ac.kr>
2021-07-06 17:17:12 +0900

    Update 118 Answer

--- Solution/[118][Easy] Pascal's Triangle.py
+++ Solution/[118][Easy] Pascal's Triangle.py
@@ -1,7 +1,5 @@
 class Solution:
     def generate(self, numRows):
-        answer = [[1] * (n+1) for n in range(numRows)]
-        for i in range(2, len(answer)):
-            for j in range(1, len(answer[i])-1):
-                answer[i][j] = answer[i-1][j-1] + answer[i-1][j]
+        import math
+        answer = [[math.comb(i, j) for j in range(i+1)] for i in range(numRows)]
         return answer
