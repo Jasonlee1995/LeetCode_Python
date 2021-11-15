38a9ffb8732433d2affd324ada82abfe46a0a009
Jason Lee <2014145110@yonsei.ac.kr>
2021-11-15 22:11:40 +0900

    Change Solution

--- Solution/[543][Easy] Diameter of Binary Tree.py
+++ Solution/[543][Easy] Diameter of Binary Tree.py
@@ -1,6 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def reverseStr(self, s, k):
-        s = list(s)
-        for idx in range(0, len(s), 2*k):
-            s[idx:idx+k] = reversed(s[idx:idx+k])
-        return ''.join(s)
+    def diameterOfBinaryTree(self, root):
+        answer, _, _ = self.recursion(root)
+        return answer
+
+    def recursion(self, node):
+        if node:
+            total_l, left_l, right_l = self.recursion(node.left)
+            total_r, left_r, right_r = self.recursion(node.right)
+            left, right = 1 + max(left_l, right_l), 1 + max(left_r, right_r)
+            total = max(total_l, total_r, left+right)
+            return total, left, right
+        return -1, -1, -1
