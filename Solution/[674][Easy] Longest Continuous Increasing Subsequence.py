318d6b95ac2c22b84a4d177a84842db53f387394
Jason Lee <2014145110@yonsei.ac.kr>
2022-01-14 15:16:16 +0900

    Delete answers

--- Solution/[674][Easy] Longest Continuous Increasing Subsequence.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def findLengthOfLCIS(self, nums):
-        answer, save_idx = 1, 0
-        for idx in range(1, len(nums)):
-            if nums[idx] <= nums[idx-1]:
-                save_idx = idx
-            answer = max(answer, idx-save_idx+1)
-        return answer
