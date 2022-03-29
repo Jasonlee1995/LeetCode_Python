19d5938731b8b06b44df7aaa546e7ab57c48572b
Jason Lee <2014145110@yonsei.ac.kr>
2022-03-29 16:06:02 +0900

    1day_1commit_easy

---
+++ Solution/[766][Easy] Toeplitz Matrix.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isToeplitzMatrix(self, matrix):
+        m, n = len(matrix), len(matrix[0])
+        for r in range(1, m):
+            for c in range(1, n):
+                if matrix[r][c] != matrix[r-1][c-1]:
+                    return False
+        return True
