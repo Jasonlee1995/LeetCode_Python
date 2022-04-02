269518d36af85be4d1ba6b65f1deafa07fd7865b
Jason Lee <2014145110@yonsei.ac.kr>
2022-04-02 16:55:41 +0900

    1day_1commit_easy

---
+++ Solution/[783][Easy] Minimum Distance Between BST Nodes.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDiffInBST(self, root):
+        def inorder(node):
+            order = []
+            if node.left: order += inorder(node.left)
+            order.append(node.val)
+            if node.right: order += inorder(node.right)
+            return order
+
+        vals = inorder(root)
+        answer = min(vals[i+1] - vals[i] for i in range(len(vals)-1))
+        return answer
