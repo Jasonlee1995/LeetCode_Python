fb671480f17b1b2418a88058d160db89ad2296f3
Jason Lee <2014145110@yonsei.ac.kr>
2022-05-15 21:35:49 +0900

    1day_1commit_easy

---
+++ Solution/[896][Easy] Monotonic Array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isMonotonic(self, nums):
+        increasing = decreasing = True
+        for i in range(len(nums)-1):
+            if nums[i] > nums[i+1]: increasing = False
+            if nums[i] < nums[i+1]: decreasing = False
+        return increasing or decreasing
